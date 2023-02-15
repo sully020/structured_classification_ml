0764ffe054655123c97a0f169bb30b4d01411fda
Chris Sullivan <cs590919@wne.edu>
2023-02-14 22:38:52 -0500

    MODIFY: main.py, create decision tree

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
 import pandas as pan
+from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
 
-def classify_hams()
+def classify_emails():
+
+    emails = pan.read_csv('emails.csv')
+    dtree = DecisionTreeClassifier(criterion = 'entropy')
+    print(emails[0])
+
+classify_emails()
