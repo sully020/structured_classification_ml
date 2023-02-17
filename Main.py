59dca6fb6c9009b066b08a4d00a4213e08ae375d
Chris Sullivan <cs590919@wne.edu>
2023-02-16 22:07:55 -0500

    ADD: Seperated dataframes into inp's and out's, added print tests

--- Main.py
+++ Main.py
@@ -3,11 +3,21 @@ from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
-
-def classify_emails():
-
+def decisiontree_emails():
+    # This will be a separate atomized function
     emails = pan.read_csv('emails.csv')
     dtree = DecisionTreeClassifier(criterion = 'entropy')
-    print(emails[0])
 
-classify_emails()
+    x = emails.drop(columns = ['Email No.', 'class_val'])
+    y = emails['class_val']
+    print(x.head(5))
+
+def decisiontree_patients():
+    patients = pan.read_csv('diabetes.csv')
+    x = patients.drop(columns = 'class_val')
+    y = patients['class_val']
+    print(x.head(5))
+
+decisiontree_emails()
+decisiontree_patients()
+
