025a91ba2fa395fa1722f0000e01431e350599e5
Chris Sullivan <cs590919@wne.edu>
2023-02-16 22:24:33 -0500

    MODIFY: Edit function names, organize code layout

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
-def decisiontree_emails():
-    # This will be a separate atomized function
+def create_emails_df():
     emails = pan.read_csv('emails.csv')
-    dtree = DecisionTreeClassifier(criterion = 'entropy')
-
     x = emails.drop(columns = ['Email No.', 'class_val'])
     y = emails['class_val']
-    print(x.head(5))
+    return x, y
+    #print(x.head(5))
 
-def decisiontree_patients():
+def create_patient_df():
     patients = pan.read_csv('diabetes.csv')
     x = patients.drop(columns = 'class_val')
     y = patients['class_val']
-    print(x.head(5))
+    return x, y
+    #print(x.head(5))
 
-decisiontree_emails()
-decisiontree_patients()
+#dtree = DecisionTreeClassifier(criterion = 'entropy')
 
