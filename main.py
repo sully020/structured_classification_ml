fe9e8153f8a931cbc2e6e0fe887ed90283279391
Chris Sullivan <cs590919@wne.edu>
2023-02-19 19:26:11 -0500

    ADD: Add decision tree fitting and scoring

--- main.py
+++ main.py
@@ -10,18 +10,25 @@ def create_emails_df():
     return x, y
     #print(x.head(5))
 
-def create_patient_df():
+def create_patients_df():
     patients = pan.read_csv('diabetes.csv')
     x = patients.drop(columns = 'class_val')
     y = patients['class_val']
     return x, y
     #print(x.head(5))
 
-def separate_data():
-    x, y = create_emails_df
-    cross_val_score(X=x, y=y)
-
 def decision_tree_emails():
+    x, y = create_emails_df()
     dtree = DecisionTreeClassifier(criterion = 'entropy', max_depth = 20, max_features = "sqrt") # ~lg(features) decisions with padding, 
-    dtree.fit(X=x, y=y)                                                                                     # semi-aggressive pruning(?)
+    scores = cross_val_score(dtree, X=x, y=y)                                                    # semi-aggressive pruning(?)
+    print(scores)
 
+def decision_tree_patients():
+    x, y = create_patients_df()
+    dtree = DecisionTreeClassifier(criterion = 'entropy', max_depth = 20, max_features = "sqrt") # ~lg(features) decisions with padding, 
+    scores = cross_val_score(dtree, X=x, y=y)                                                    # semi-aggressive pruning(?)
+    print(scores)
+
+def test_decision_trees():
+    decision_tree_emails()
+    decision_tree_patients()
