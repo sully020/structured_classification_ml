411f911af0c926fcf0294fa91944276d02ae3680
Chris Sullivan <cs590919@wne.edu>
2023-02-25 23:36:21 -0500

    ADD: Add test error output function.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pan
 import seaborn as sea
 import numpy as np
 from matplotlib import pyplot as plt
-from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.model_selection import cross_validate, cross_val_predict
 from sklearn.metrics import confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
@@ -16,45 +16,45 @@ def create_emails_dframe():
     return x, y
 
 def choose_email_learner(choice):
-    cv = 5
     match choice:
         case 'decision tree':
             # weights = {i: 3000 - i for i in range(3000)}  // deprecated, 'balanced' further increases accuracy.
-            learner = DecisionTreeClassifier(class_weight = 'balanced') # DONE
-            cv = 10
+            learner = DecisionTreeClassifier(class_weight = 'balanced')
         case 'neural network':
-            learner = MLPClassifier() # TODO, hidden layers last
+            learner = MLPClassifier()
         case 'k neighbors':
-            learner = KNeighborsClassifier(20, weights = 'distance', p = 1) # DONE
+            learner = KNeighborsClassifier(20, weights = 'distance', p = 1)
         case 'random forest':
-            learner = RandomForestClassifier() # TODO
+            learner = RandomForestClassifier(class_weight = 'balanced', random_state=2)
         case _:
             raise ValueError("Inappropriate argument value of " + str(choice) + " entered.")
 
-    return learner, cv
+    return learner
 
-def score_email_predictions(learner, cv):
+def score_email_predictions(learner):
     x, y = create_emails_dframe()
-    scores = cross_val_score(learner, x, y, cv = cv)                      
+    scores = cross_validate(learner, x, y, cv = 10, return_train_score = True)                      
     return scores
 
-def generate_email_heatmap(learner, cv):
+def generate_email_heatmap(learner):
     x, y = create_emails_dframe()
-    preds = cross_val_predict(learner, x, y, cv = cv) 
+    preds = cross_val_predict(learner, x, y, cv = 10) 
     cf_matrix = confusion_matrix(y, preds)    
-    sea.heatmap(cf_matrix / np.sum(cf_matrix), annot=True, fmt='.2%', cmap='Reds')
+    sea.heatmap(cf_matrix / np.sum(cf_matrix), annot=True, fmt='.2%', cmap=('YlOrRd'))
     plt.show()
 
 def model_learning_on_emails():
     choice = input("Please type your choice of learning model for the spam e-mails dataset: \
 1.) 'decision tree' 2.) 'neural network' 3.) 'k neighbors' 4.) 'random forest': ")
-    learner, cv = choose_email_learner(choice)
-    email_scores = score_email_predictions(learner, cv)
+    learner = choose_email_learner(choice)
+    email_scores = score_email_predictions(learner)
 
     print("The mean accuracy of the " + choice + " used to classify spam e-mails by " 
-    + str(cv) + "-fold cross-validation is: " + "{:.2f}".format(email_scores.mean() * 100) + "%") #CHANGE TO .0f after analysis
+    + "10-fold cross-validation is: " + "{:.2f}".format(email_scores['test_score'].mean() * 100) + "%\n")
 
-    generate_email_heatmap(learner, cv)
+    print("The mean accuracy of the " + choice + " during training was: " + "{:.2f}".format(email_scores['train_score'].mean() * 100) + "%")
+
+    generate_email_heatmap(learner)
 
 
 
@@ -65,16 +65,19 @@ def create_patients_dframe():
     return x, y
 
 def choose_patients_learner(choice):
+    # CV 5 produces better results for some of these
     cv = 5
     match choice:
         case 'decision tree':
-            learner = DecisionTreeClassifier(max_depth = 5) # DONE
+            learner = DecisionTreeClassifier(max_depth = 5, max_leaf_nodes = 21, random_state = 1)
+            cv = 10
         case 'neural network':
-            learner = MLPClassifier(67, learning_rate_init = .04, random_state = 0) # DONE
+            learner = MLPClassifier(67, learning_rate_init = .04, random_state = 1)
+            cv = 10
         case 'k neighbors':
-            learner = KNeighborsClassifier(19, weights = 'distance', p = 1) # DONE, weights and p only increase by tenths of percentage, not whole percentages like emails
+            learner = KNeighborsClassifier(19, weights = 'distance', p = 1) # weights and p only increase by tenths of percentage, not whole percentages like emails
         case 'random forest':
-            learner = RandomForestClassifier() # TODO
+            learner = RandomForestClassifier(random_state = 0)
         case _:
             raise ValueError("Inappropriate argument value of " + str(choice) + " entered.")
 
@@ -82,14 +85,14 @@ def choose_patients_learner(choice):
 
 def score_patients_predictions(learner, cv):
    x, y = create_patients_dframe()
-   scores = cross_val_score(learner, x, y, cv = cv)                      
+   scores = cross_validate(learner, x, y, cv = cv, return_train_score = True)                      
    return scores
 
 def generate_patients_heatmap(learner, cv):
     x, y = create_patients_dframe()
     preds = cross_val_predict(learner, x, y, cv = cv)
     cf_matrix = confusion_matrix(y, preds)    
-    sea.heatmap(cf_matrix / np.sum(cf_matrix), annot=True, fmt='.2%', cmap='Reds')
+    sea.heatmap(cf_matrix / np.sum(cf_matrix), annot=True, fmt='.2%', cmap='Oranges')
     plt.show()
 
 def model_learning_on_patients():
@@ -100,7 +103,9 @@ def model_learning_on_patients():
     patients_scores = score_patients_predictions(learner, cv)
 
     print("The mean accuracy of the " + choice + " used to classify diabetes by " \
-    + str(cv) + " fold cross-validation is: " + "{:.2f}".format(patients_scores.mean() * 100) + "%")
+    + str(cv) + " fold cross-validation is: " + "{:.2f}".format(patients_scores['test_score'].mean() * 100) + "%\n")
+
+    print("The mean accuracy of the " + choice + " during training was " + "{:.2f}".format(patients_scores['train_score'].mean() * 100) + "%")
 
     generate_patients_heatmap(learner, cv)
 
